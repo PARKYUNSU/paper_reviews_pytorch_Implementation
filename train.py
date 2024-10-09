bb2dfa51e670f99b26748244789e74d7cfb4012b
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-09 14:40:59 +0900

    train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 
 def model_train(model, data_loader, criterion, optimizer, epoch, device):
     model.train()
-    running_loss, correct = 0.0, 0.0
+    running_loss, correct_top1, correct_top5 = 0.0, 0.0, 0.0
     total = len(data_loader.dataset)
     
     pbar = tqdm(data_loader)
@@ -17,9 +17,17 @@ def model_train(model, data_loader, criterion, optimizer, epoch, device):
         optimizer.step()
         
         running_loss += loss.item() * images.size(0)
-        correct += (outputs.argmax(1) == labels).sum().item()
+        
+        # Top-1 & Top-5 Accuracy 계산
+        _, pred_top1 = outputs.topk(1, 1, True, True)  # Top-1 예측
+        _, pred_top5 = outputs.topk(5, 1, True, True)  # Top-5 예측
+        
+        correct_top1 += pred_top1.eq(labels.view(-1, 1)).sum().item()  # Top-1 예측과 실제 값 비교
+        correct_top5 += pred_top5.eq(labels.view(-1, 1).expand_as(pred_top5)).sum().item()  # Top-5 예측과 비교
     
     avg_loss = running_loss / total
-    avg_accuracy = correct / total
-    
-    return avg_loss, avg_accuracy
+    top1_accuracy = 100 * correct_top1 / total  # 정확도 계산 (에러율 아님)
+    top1_error_rate = 100 * (1 - correct_top1 / total)
+    top5_error_rate = 100 * (1 - correct_top5 / total)
+
+    return avg_loss, top1_accuracy, top1_error_rate, top5_error_rate
