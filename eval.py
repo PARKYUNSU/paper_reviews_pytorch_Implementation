acc60ad547a0b95bb1d20a79cc53a53c6aecb841
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-22 01:20:26 +0900

    eval.py

--- eval.py
+++ eval.py
@@ -1,31 +1,21 @@
 import torch
-from utils import intersection_and_union
+from tqdm import tqdm
+from utils import build_metrics
 
-def model_val(model, data_loader, criterion, device, num_classes):
+def model_val(model, data_loader, device, num_classes):
     model.eval()
     running_loss = 0.0
-    intersection_meter, union_meter = 0.0, 0.0
-
+    running_mIoU = 0.0
     total = len(data_loader.dataset)
 
     with torch.no_grad():
-        for images, labels in data_loader:
-            images, labels = images.to(device), labels.to(device)
+        for batch in tqdm(data_loader, desc="Validation"):
+            # build_metrics 사용하여 손실, 정확도, mIoU 계산
+            loss_seg, accuracy, mIoU = build_metrics(model, batch, device, num_classes)
 
-            outputs = model(images)  # 모델 예측
-            loss = criterion(outputs, labels)
-            running_loss += loss.item() * images.size(0)
-
-            outputs_np = outputs.detach().cpu().numpy()
-            labels_np = labels.cpu().numpy()
-
-            # mIoU 계산
-            intersection, union = intersection_and_union(outputs_np, labels_np, num_classes)
-            intersection_meter += intersection
-            union_meter += union
+            running_loss += loss_seg.item()
+            running_mIoU += mIoU
 
     avg_loss = running_loss / total
-    iou = intersection_meter / (union_meter + 1e-10)
-    mIoU = np.mean(iou)  # Mean IoU 계산
-
-    return avg_loss, mIoU
+    avg_mIoU = running_mIoU / len(data_loader)
+    return avg_loss, avg_mIoU
