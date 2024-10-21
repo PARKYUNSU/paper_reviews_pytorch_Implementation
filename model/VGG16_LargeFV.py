1b11784e8ea5b864e94d237a6d36b770d8d0737b
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-21 22:40:25 +0900

    VGG16_LargeFV

---
+++ model/VGG16_LargeFV.py
@@ -0,0 +1,106 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class VGG16_LargeFV(nn.Module):
+    def __init__(self, num_classes=21, input_size=321, split='train', init_weights=True):
+        super(VGG16_LargeFV, self).__init__()
+        self.input_size = input_size
+        self.split = split
+        
+        self.features = nn.Sequential(
+            # Conv1
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            # Conv2
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            # Conv3
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            # Conv4
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+
+            # Conv5 (Dilatied Convolution)
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=2, dilation=2),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=2, dilation=2),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=2, dilation=2),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        )
+
+        # Fully convolutional layers (fc6 and fc7)
+        self.fc6 = nn.Sequential(
+            nn.Conv2d(512, 1024, kernel_size=3, padding=12, dilation=12),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(0.5)
+        )
+        self.fc7 = nn.Sequential(
+            nn.Conv2d(1024, 1024, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(0.5)
+        )
+        
+        # classification layer
+        self.classifier = nn.Conv2d(1024, num_classes, kernel_size=1)
+
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.fc6(x)
+        x = self.fc7(x)
+        x = self.classifier(x)
+
+        # Upsampling
+        x = F.interpolate(x, size=(self.input_size, self.input_size), mode='bilinear', align_corners=True)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_uniform_(m.weight, mode='fan_in', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+if __name__ == "__main__":
+    model = VGG16_LargeFV()
+    input_tensor = torch.ones([2, 3, 321, 321])
+    output = model(input_tensor)
+    print(output.shape)
