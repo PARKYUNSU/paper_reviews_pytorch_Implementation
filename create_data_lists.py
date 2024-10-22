33874549fc206993a7772555f4cab83f3e9874ab
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-22 09:46:39 +0900

    create_data_lists.py

--- create_data_lists.py
+++ create_data_lists.py
@@ -1,6 +1,6 @@
-from utils import create_data_lists
+from utils.utils import create_segmentation_data_lists
 
 if __name__ == '__main__':
-    create_data_lists(voc07_path='/kaggle/input/pascal/archive/VOC2007',
+    create_segmentation_data_lists(voc07_path='/kaggle/input/pascal/archive/VOC2007',
                       voc12_path='/kaggle/input/pascal/archive/VOC2012',
                       output_folder='/kaggle/working/')
