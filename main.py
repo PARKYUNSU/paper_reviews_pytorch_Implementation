9a14c2e65253f621312a3e9a1c900750814839ac
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-22 13:45:12 +0900

    maiin.py

--- main.py
+++ main.py
@@ -27,22 +27,30 @@ def main():
     val_image_transform, val_mask_transform = config.get_transforms(input_size=321)
 
     train_dataset = PascalVOCSegmentationDataset(
-        data_folder='path_to_dataset', split='train',
+        data_folder='/kaggle/working',  # 데이터 파일들이 저장된 경로
+        split='train',  # 훈련용 데이터를 사용할 때 'train'
         transform=lambda img, mask: (train_image_transform(img), train_mask_transform(mask))
     )
-    val_dataset = PascalVOCSegmentationDataset(
-        data_folder='path_to_dataset', split='val',
+
+    test_dataset = PascalVOCSegmentationDataset(
+        data_folder='/kaggle/working',  # 데이터 파일들이 저장된 경로
+        split='test',  # 검증 또는 테스트용 데이터를 사용할 때 'test'
         transform=lambda img, mask: (val_image_transform(img), val_mask_transform(mask))
     )
 
     train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, collate_fn=train_dataset.collate_fn)
-    val_loader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=False, collate_fn=val_dataset.collate_fn)
+    val_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False, collate_fn=test_dataset.collate_fn)
 
     # Train
+
     for epoch in range(config.num_epochs):
-        train_loss, train_miou = model_train(model, train_loader, criterion, optimizer, device, scaler, num_classes=21)
-        val_loss, val_miou = model_val(model, val_loader, criterion, device, num_classes=21)
+        # epoch 인자를 추가하여 호출
+        train_loss, train_miou = model_train(model, train_loader, epoch, optimizer, device)
+        val_loss, val_miou = model_val(model, val_loader, criterion, device)
 
         print(f'Epoch [{epoch+1}/{config.num_epochs}], '
-              f'Train Loss: {train_loss:.4f}, Train mIoU: {train_miou:.4f}, '
-              f'Val Loss: {val_loss:.4f}, Val mIoU: {val_miou:.4f}')
+            f'Train Loss: {train_loss:.4f}, Train mIoU: {train_miou:.4f}, '
+            f'Val Loss: {val_loss:.4f}, Val mIoU: {val_miou:.4f}')
+
+if __name__ == "__main__":
+    main()
