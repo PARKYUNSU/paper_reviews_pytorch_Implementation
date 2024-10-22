b1d372924fa16d3eb71c746b4af27923d99ab765
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-22 09:46:46 +0900

    main.py

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-import config # 하이퍼파라미터 및 transform 설정
+import config
 from datasets import PascalVOCSegmentationDataset
 from model import VGG16_LargeFV
 from train import model_train
 from eval import model_val
 
 def main():
-    # Device 설정
+    # Device
     device = config.device
 
-    # 모델 초기화
+    # Model
     model = VGG16_LargeFV(num_classes=21, input_size=321).to(device)
     
-    # 손실 함수 및 옵티마이저 설정
+    # criterion, optimizer
     criterion = nn.CrossEntropyLoss(ignore_index=255).to(device)
     optimizer = optim.SGD(model.parameters(), lr=config.initial_lr, momentum=config.momentum, weight_decay=config.weight_decay)
     
-    # Mixed Precision Training 설정
+    # Mixed Precision Training(Fp16)
     scaler = torch.cuda.amp.GradScaler()
 
-    # 데이터셋 및 데이터로더 설정
+    # Dataset Load
     train_image_transform, train_mask_transform = config.get_transforms(input_size=321)
     val_image_transform, val_mask_transform = config.get_transforms(input_size=321)
 
@@ -38,7 +38,7 @@ def main():
     train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, collate_fn=train_dataset.collate_fn)
     val_loader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=False, collate_fn=val_dataset.collate_fn)
 
-    # 학습 시작
+    # Train
     for epoch in range(config.num_epochs):
         train_loss, train_miou = model_train(model, train_loader, criterion, optimizer, device, scaler, num_classes=21)
         val_loss, val_miou = model_val(model, val_loader, criterion, device, num_classes=21)
