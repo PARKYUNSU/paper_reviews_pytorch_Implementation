66337dc97756015b4616cc66bf9b5eb7760bff37
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-28 12:26:32 +0900

    VGG16 FCN Pretrained

--- model/vgg16_fcn_pre.py
+++ model/vgg16_fcn_pre.py
@@ -1,36 +1,55 @@
 import torch
 import torch.nn as nn
-import torchvision.models as models
+from torchvision.models import vgg16
 
-class FCN32s(nn.Module):
-    def __init__(self, num_classes):
-        super(FCN32s, self).__init__()
-        
-        # Pretrained VGG16 모델을 불러오고 Fully Connected layer들을 제거
-        vgg16 = models.vgg16(pretrained=True)
-        features = list(vgg16.features.children())
-        self.features = nn.Sequential(*features)
-        self.score = nn.Conv2d(512, num_classes, kernel_size=1)
+class FCN8s(nn.Module):
+    def __init__(self, num_classes=32):  # CamVid 클래스 수에 맞춤
+        super(FCN8s, self).__init__()
+        self.vgg = vgg16(weights='IMAGENET1K_V1').features  # pretrained=True 대신 weights 사용
+        self.f3, self.f4, self.f5 = 16, 23, 30  # Layer indices for VGG
 
-        # 최종 출력을 32배 업샘플링 (FCN-32s)
-        self.upsample32x = nn.ConvTranspose2d(num_classes, num_classes, kernel_size=64, stride=32, padding=16, bias=False)
-    
-        self._initialize_weights()
+        # Convolutional layers replacing the dense layers
+        self.f5_conv1 = nn.Conv2d(512, 4096, kernel_size=7, padding=3)
+        self.f5_conv2 = nn.Conv2d(4096, 4096, kernel_size=1)
+        self.f5_conv3 = nn.Conv2d(4096, num_classes, kernel_size=1)
+
+        # Transposed convolutions for upsampling
+        self.upsample_f5 = nn.ConvTranspose2d(num_classes, num_classes, kernel_size=4, stride=2, padding=1, bias=False)
+        self.upsample_f4 = nn.ConvTranspose2d(num_classes, num_classes, kernel_size=4, stride=2, padding=1, bias=False)
+        self.upsample_final = nn.ConvTranspose2d(num_classes, num_classes, kernel_size=8, stride=4, padding=2, bias=False)
+
+        # f4와 f3를 num_classes 채널로 변환
+        self.f4_conv1 = nn.Conv2d(512, num_classes, kernel_size=1)
+        self.f3_conv1 = nn.Conv2d(256, num_classes, kernel_size=1)
 
     def forward(self, x):
-        x = self.features(x)
-        x = self.score(x)
-        # 32배 업샘플링하여 원래 이미지 크기로 복원
-        x = self.upsample32x(x)
+        f3 = self.vgg[:self.f3](x)
+        f4 = self.vgg[self.f3:self.f4](f3)
+        f5 = self.vgg[self.f4:self.f5](f4)
+
+        f5 = self.f5_conv1(f5)
+        f5 = nn.ReLU(inplace=True)(f5)
+        f5 = nn.Dropout(p=0.5)(f5)  # 드롭아웃 확률 0.5
+        f5 = self.f5_conv2(f5)
+        f5 = nn.ReLU(inplace=True)(f5)
+        f5 = nn.Dropout(p=0.5)(f5)
+        f5 = self.f5_conv3(f5)
+
+        upsampled_f5 = self.upsample_f5(f5)
+
+        # f4 채널을 num_classes로 변환
+        f4 = self.f4_conv1(f4)
+        f4 = self.upsample_f4(f4 + upsampled_f5)
+
+        # f3 채널을 num_classes로 변환
+        f3 = self.f3_conv1(f3)
+        f3 = self.upsample_final(f4 + f3)
         
-        return x
-    
-    def _initialize_weights(self):
-        nn.init.xavier_normal_(self.score.weight)
-        nn.init.xavier_normal_(self.upsample32x.weight)
+        return f3
 
-model = FCN32s(num_classes=21)
-input_tensor = torch.randn(1, 3, 224, 224)
-output = model(input_tensor)
-
-print(output.shape)
+# 모델 초기화 및 테스트
+if __name__ == "__main__":
+    model = FCN8s(num_classes=32)  # CamVid 데이터셋에 맞춤
+    input = torch.ones([1, 3, 224, 224])
+    output = model(input)
+    print(f"Final shapes - output: {output.shape}")
