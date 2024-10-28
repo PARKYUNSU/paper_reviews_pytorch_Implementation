5a3f13e00e98f3704ff0f79e150df4797b39570b
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-28 15:05:45 +0900

    Delete create_data_lists.py

--- create_data_lists.py
+++
@@ -1,6 +0,0 @@
-from utils.utils import create_segmentation_data_lists
-
-if __name__ == '__main__':
-    create_segmentation_data_lists(voc07_path='/kaggle/input/voc0712/VOC_dataset/VOCdevkit/VOC2007',
-                      voc12_path='/kaggle/input/voc0712/VOC_dataset/VOCdevkit/VOC2012',
-                      output_folder='/kaggle/working/')
