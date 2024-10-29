329574a9f7be1618d20084c171295e27898f7835
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-29 09:19:27 +0900

    CRF

--- utils/crf.py
+++ utils/crf.py
@@ -1,23 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-#
-# Author: Kazuto Nakashima
-# URL:    https://kazuto1011.github.io
-# Date:   09 January 2019
-
-
 import numpy as np
-import pydensecrf.densecrf as dcrf
-import pydensecrf.utils as utils
-
+import cv2
 
 class DenseCRF(object):
     def __init__(self, iter_max, pos_w, pos_xy_std, bi_w, bi_xy_std, bi_rgb_std):
         """
-        DenseCRF 클래스 초기화.
-        
         Args:
-        iter_max (int): CRF 추론 반복 횟수
+        iter_max (int): CRF-like 추론 반복 횟수
         pos_w (float): 가우시안 페어와이즈 항 가중치
         pos_xy_std (float): 가우시안 필터에서 위치 차이 표준 편차 (spatial distance standard deviation)
         bi_w (float): 양방향 필터 가중치
@@ -33,45 +21,36 @@ class DenseCRF(object):
 
     def __call__(self, image, probmap):
         """
-        CRF 추론 수행. 입력 이미지와 모델의 소프트맥스 확률 맵을 사용해 픽셀 단위 클래스 확률을 계산.
+        CRF-like 후처리 수행. 입력 이미지와 모델의 소프트맥스 확률 맵을 사용해 픽셀 단위 클래스 확률을 계산.
         
         Args:
         image (np.ndarray): 원본 이미지 (H, W, 3)
         probmap (np.ndarray): 모델이 출력한 소프트맥스 확률 맵 (C, H, W) - 클래스별 확률
 
         Returns:
-        Q (np.ndarray): CRF 후처리된 확률 맵 (C, H, W)
+        Q (np.ndarray): CRF-like 후처리된 확률 맵 (C, H, W)
         """
         C, H, W = probmap.shape
+        refined_probmap = np.zeros_like(probmap)
 
-        # U : Unray Energy
-        U = utils.unary_from_softmax(probmap)
-        U = np.ascontiguousarray(U) # memory 레이아웃을 연속적인 배열로 변환 (CRF에 맞게 사용)
+        # 각 클래스에 대해 CRF-like 후처리 적용
+        for c in range(C):
+            class_map = probmap[c]
+            
+            # 가우시안 필터 적용
+            gaussian_filtered = cv2.GaussianBlur(class_map, (0, 0), self.pos_xy_std) * self.pos_w
+            
+            # 양방향 필터 적용
+            bilateral_filtered = cv2.bilateralFilter((class_map * 255).astype(np.uint8), 
+                                                     d=0, 
+                                                     sigmaColor=self.bi_rgb_std, 
+                                                     sigmaSpace=self.bi_xy_std)
+            bilateral_filtered = (bilateral_filtered / 255.0) * self.bi_w
+            
+            # 두 필터 결과를 합산하여 후처리 결과 생성
+            refined_probmap[c] = gaussian_filtered + bilateral_filtered
 
-        # 입력 이미지를 연속된 배열로 변환
-        image = np.ascontiguousarray(image)
-
-        # d : DenseCRF2D 객체 초기화 (이미지 크기 및 클래스 수 지정)
-        d = dcrf.DenseCRF2D(W, H, C)
+        # 클래스별 확률 맵을 softmax-like 형태로 조정
+        Q = np.exp(refined_probmap) / np.sum(np.exp(refined_probmap), axis=0)
         
-        # Unary Energy 설정
-        d.setUnaryEnergy(U)
-        
-        # Pairwise Energy 추가: 가우시안 커널을 사용하여 공간적 관계를 고려
-        # 공간적으로 가까운 픽셀들이 비슷한 클래스로 분류되도록 유도
-        d.addPairwiseGaussian(sxy=self.pos_xy_std, compat=self.pos_w)
-        
-        # Pairwise Energy 추가: 양방향 필터 사용 (공간적 거리 및 색상 정보를 동시에 고려)
-        # 공간적으로 가깝고 색상이 비슷한 픽셀들이 같은 클래스로 분류되도록 유도        
-        d.addPairwiseBilateral(
-            sxy=self.bi_xy_std, # 거리 차이의 표준편차
-            srgb=self.bi_rgb_std, # 색깔 차이의 표준편차
-            rgbim=image, # 원본이미지
-            compat=self.bi_w # 양방향 필터 가중치
-        )
-        # Q : 확률 분포 (최종 CRF 추론 결과)
-        # 픽셀마다 각 클래스에 대한 확률 분포)
-        Q = d.inference(self.iter_max) # 추론 반복 횟수
-        Q = np.array(Q).reshape((C, H, W)) # C H W 재배열
-
         return Q
