ed67603f9c253b3a9d66d26ff4e1cacad9f05a11
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-30 14:13:02 +0900

    SE block

--- model/se_block.py
+++ model/se_block.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import nn
+from torch import nn
 
 class SE_block(nn.Module):
     def __init__(self, in_channels, reduction_ratio=4): # MobilenetV3 r = 4
@@ -18,7 +18,5 @@ class SE_block(nn.Module):
     
     def forward(self, x):
         se_weight = self.squeeze(x)  # (N, C, H, W) -> (N, C, 1, 1)
-        se_weight = se_weight.view(se_weight.size(0), -1)  # (N, C, 1, 1) -> (N, C)로 reshape
         se_weight = self.excitation(se_weight)  # 채널별 중요도 계산
-        se_weight = se_weight.view(se_weight.size(0), se_weight.size(1), 1, 1)  # (N, C) -> (N, C, 1, 1)로 다시 reshape
         return x * se_weight  # 입력에 중요도를 반영하여 조정
