b0bc4a0e90feaa8ecccbf9b5189684eea4d682d6
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-10-31 15:52:38 +0900

    ResNet (pretrained)

---
+++ model/resnet_pre.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+from torchinfo import summary
+
+def initialize_weights(*models):
+    for model in models:
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data, nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1.)
+                m.bias.data.fill_(1e-4)
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0.0, 0.0001)
+                m.bias.data.zero_()
+
+
+class ResNet(nn.Module):
+    def __init__(self, in_channels=3, output_stride=16, backbone='resnet101', pretrained=True):
+        super(ResNet, self).__init__()
+        model = getattr(models, backbone)(pretrained)
+        if not pretrained or in_channels != 3:
+            self.layer0 = nn.Sequential(
+                nn.Conv2d(in_channels, 64, 7, stride=2, padding=3, bias=False),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+            )
+            initialize_weights(self.layer0)
+        else:
+            self.layer0 = nn.Sequential(*list(model.children())[:4])
+        
+        self.layer1 = model.layer1
+        self.layer2 = model.layer2
+        self.layer3 = model.layer3
+        self.layer4 = model.layer4
+
+        if output_stride == 16: s3, s4, d3, d4 = (2, 1, 1, 2)
+        elif output_stride == 8: s3, s4, d3, d4 = (1, 1, 2, 4)
+        
+        if output_stride == 8: 
+            for n, m in self.layer3.named_modules():
+                if 'conv1' in n and (backbone == 'resnet34' or backbone == 'resnet18'):
+                    m.dilation, m.padding, m.stride = (d3,d3), (d3,d3), (s3,s3)
+                elif 'conv2' in n:
+                    m.dilation, m.padding, m.stride = (d3,d3), (d3,d3), (s3,s3)
+                elif 'downsample.0' in n:
+                    m.stride = (s3, s3)
+
+        for n, m in self.layer4.named_modules():
+            if 'conv1' in n and (backbone == 'resnet34' or backbone == 'resnet18'):
+                m.dilation, m.padding, m.stride = (d4,d4), (d4,d4), (s4,s4)
+            elif 'conv2' in n:
+                m.dilation, m.padding, m.stride = (d4,d4), (d4,d4), (s4,s4)
+            elif 'downsample.0' in n:
+                m.stride = (s4, s4)
+
+
+    def forward(self, x):
+        x = self.layer0(x)
+        x = self.layer1(x)
+        low_level_features = x
+        print("Low-level features shape:", low_level_features.shape)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        return x, low_level_features
+
+model = ResNet()
+# 모델 요약 출력
+summary(model, input_size = (2, 3, 224, 224), device = "cpu")
