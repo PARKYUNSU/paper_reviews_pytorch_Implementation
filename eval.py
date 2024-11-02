228dd76f01e12a6493a5bf854f4b515dc0507d86
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-11-02 20:10:41 +0900

    eval.py

--- eval.py
+++ eval.py
@@ -3,6 +3,8 @@ import torch
 def evaluate(model, dataloader, criterion, device):
     model.eval()
     total_loss = 0
+    corrects = 0
+    total_samples = 0
 
     with torch.no_grad():
         for images, targets in dataloader:
@@ -10,7 +12,13 @@ def evaluate(model, dataloader, criterion, device):
             outputs = model(images)
             loss = criterion(outputs, targets.squeeze(1).long())
             total_loss += loss.item()
+            
+            _, preds = torch.max(outputs, dim=1)
+            corrects += torch.sum(preds == targets.squeeze(1))
+            total_samples += targets.size(0)
     
     average_loss = total_loss / len(dataloader)
-    print(f"Evaluation complete. Average Loss: {average_loss:.4f}")
-    return average_loss
+    accuracy = (corrects / total_samples).item() * 100
+    print(f"Evaluation complete. Average Loss: {average_loss:.4f}, Accuracy: {accuracy:.2f}%")
+    
+    return average_loss, accuracy
