affd3b02c5ad4e86b5feb264bd9a18ea2699a651
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-11-02 21:07:06 +0900

    eval.py

--- eval.py
+++ eval.py
@@ -3,8 +3,6 @@ import torch
 def evaluate(model, dataloader, criterion, device):
     model.eval()
     total_loss = 0
-    corrects = 0
-    total_pixels = 0  # 픽셀 단위 정확도 계산을 위한 변수
 
     with torch.no_grad():
         for images, targets in dataloader:
@@ -12,14 +10,7 @@ def evaluate(model, dataloader, criterion, device):
             outputs = model(images)
             loss = criterion(outputs, targets.squeeze(1).long())
             total_loss += loss.item()
-            
-            # 픽셀 단위 정확도 계산
-            _, preds = torch.max(outputs, dim=1)
-            corrects += torch.sum(preds == targets.squeeze(1))
-            total_pixels += targets.numel()  # 총 픽셀 수
-
-    average_loss = total_loss / len(dataloader)
-    accuracy = (corrects / total_pixels).item() * 100  # 픽셀 기반 정확도 계산
-    print(f"Evaluation complete. Average Loss: {average_loss:.4f}, Accuracy: {accuracy:.2f}%")
     
-    return average_loss, accuracy
+    average_loss = total_loss / len(dataloader)
+    print(f"Evaluation complete. Average Loss: {average_loss:.4f}")
+    return average_loss
