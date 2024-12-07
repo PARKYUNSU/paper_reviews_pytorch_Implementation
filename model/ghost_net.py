3c134f08e61ab240e80c908955a62611d63556b1
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-12-07 22:45:05 +0900

    commit

--- model/ghost_net.py
+++ model/ghost_net.py
@@ -141,12 +141,16 @@ class GhostNet(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.BatchNorm1d)):
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
 
 def ghostnet(num_classes=80):
     cfgs = [
