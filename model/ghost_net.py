f4ce33b139b86a538e1a4ac4a8593b29c73dea11
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-12-07 22:50:54 +0900

    commit

--- model/ghost_net.py
+++ model/ghost_net.py
@@ -49,8 +49,7 @@ class SELayer(nn.Module):
 class GhostModule(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=1, ratio=2, dw_kernel_size=3, stride=1, relu=True):
         super().__init__()
-        self.oup = oup
-
+        self.oup = out_channels
         init_channels = math.ceil(out_channels / ratio)
         cheap_channels = init_channels * (ratio - 1)
 
