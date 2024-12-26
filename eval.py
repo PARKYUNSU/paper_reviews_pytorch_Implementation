cf1ce3049232891bff231413f6376cd9df9f381d
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-12-26 12:09:13 +0900

    commit

--- eval.py
+++ eval.py
@@ -8,13 +8,18 @@ def evaluate_one_epoch(model, data_loader, loss_fn, device):
         for texts, labels in data_loader:
             texts, labels = texts.to(device), labels.to(device)
 
-            model.init_hidden_and_cell_state(len(texts), device)
-            outputs = model(texts)
+            # Hidden and cell state 초기화
+            hidden = model.init_hidden_and_cell_state(batch_size=len(texts), device=device)
+
+            # Forward pass
+            outputs = model(texts, hidden)
             loss = loss_fn(outputs, labels)
 
-            running_loss += loss.item()
+            # Accuracy 계산
             preds = outputs.argmax(dim=1)
             correct += (preds == labels).sum().item()
             total += labels.size(0)
+
+            running_loss += loss.item()
 
     return running_loss / total, correct / total
