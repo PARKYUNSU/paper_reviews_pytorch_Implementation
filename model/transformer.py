286bb47f06a5a960a565603d51003b1cdb23644a
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-01-17 17:37:38 +0900

    Transformer

--- model/transformer.py
+++ model/transformer.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 from .transformer_encoder import TransformerEncoder
 from .transformer_decoder import TransformerDecoder
 
-class TransformerModel(nn.Module):
+class Transformer(nn.Module):
     def __init__(self, num_layers, d_model, num_heads, d_ff, vocab_size, max_seq_len, dropout=0.1):
-        super(TransformerModel, self).__init__()
+        super(Transformer, self).__init__()
         self.encoder = TransformerEncoder(num_layers, d_model, num_heads, d_ff, vocab_size, max_seq_len, dropout)
         self.decoder = TransformerDecoder(num_layers, d_model, num_heads, d_ff, vocab_size, max_seq_len, dropout)
-
+    
     def forward(self, src, tgt, tgt_mask=None, memory_mask=None):
         enc_output = self.encoder(src)
         output = self.decoder(tgt, enc_output, tgt_mask=tgt_mask, memory_mask=memory_mask)
@@ -27,14 +27,21 @@ if __name__ == "__main__":
     src_seq_len = 10  # 소스 시퀀스 길이 (인코더 입력)
     tgt_seq_len = 8  # 타겟 시퀀스 길이 (디코더 입력)
 
-    model = TransformerModel(num_layers, d_model, num_heads, d_ff, vocab_size, max_seq_len, dropout)
+    model = Transformer(num_layers, d_model, num_heads, d_ff, vocab_size, max_seq_len, dropout)
 
     src = torch.randint(0, vocab_size, (batch_size, src_seq_len))  # (batch_size, src_seq_len)
     tgt = torch.randint(0, vocab_size, (batch_size, tgt_seq_len))  # (batch_size, tgt_seq_len)
 
+    def generate_memory_mask(src_seq_len, tgt_seq_len, device='cpu'):
+
+        mask = torch.zeros(tgt_seq_len, src_seq_len, device=device)
+        mask = mask.masked_fill(mask == 0, float('-inf'))  # 마스크 적용
+        return mask.unsqueeze(0)  # (1, tgt_seq_len, src_seq_len) 형태로 반환
+
     # 마스크 생성
     tgt_mask = model.decoder.self_attention.generate_square_subsequent_mask(tgt_seq_len).unsqueeze(0)  # (1, tgt_seq_len, tgt_seq_len)
-    memory_mask = None
+    memory_mask = generate_memory_mask(src_seq_len, tgt_seq_len, device=src.device)
+
 
     output = model(src, tgt, tgt_mask=tgt_mask, memory_mask=memory_mask)
 
