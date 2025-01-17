e22e54f642322aa6df476bc0ebc72e05b7577e60
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-01-17 18:01:04 +0900

    train.py

--- train.py
+++ train.py
@@ -1,19 +1,21 @@
 import torch
 
-def train_loop(model, opt, loss_fn, dataloader):
+def train_loop(model, opt, loss_fn, dataloader, device):
     model.train()
     total_loss = 0
-
     for batch in dataloader:
         X, y = batch[:, 0], batch[:, 1]
         X, y = torch.tensor(X).to(device), torch.tensor(y).to(device)
 
+        # 이제 tgt를 1만큼 이동하여 <SOS>를 사용하여 pos 1에서 토큰을 예측
         y_input = y[:, :-1]
         y_expected = y[:, 1:]
 
+        # 다음 단어를 마스킹하려면 마스크 가져오기
         sequence_length = y_input.size(1)
         tgt_mask = model.get_tgt_mask(sequence_length).to(device)
 
+        # X, y_input 및 tgt_mask를 전달하여 표준 training
         pred = model(X, y_input, tgt_mask)
         pred = pred.permute(1, 2, 0)
         loss = loss_fn(pred, y_expected)
@@ -26,7 +28,7 @@ def train_loop(model, opt, loss_fn, dataloader):
 
     return total_loss / len(dataloader)
 
-def validation_loop(model, loss_fn, dataloader):
+def validation_loop(model, loss_fn, dataloader, device):
     model.eval()
     total_loss = 0
 
@@ -48,19 +50,21 @@ def validation_loop(model, loss_fn, dataloader):
 
     return total_loss / len(dataloader)
 
-def fit(model, opt, loss_fn, train_dataloader, val_dataloader, epochs):
+def fit(model, opt, loss_fn, train_dataloader, val_dataloader, epochs, device):
     train_loss_list, validation_loss_list = [], []
 
+    print("Training and validating model")
     for epoch in range(epochs):
-        print(f"Epoch {epoch + 1}")
+        print("-" * 25, f"Epoch {epoch + 1}", "-" * 25)
 
-        train_loss = train_loop(model, opt, loss_fn, train_dataloader)
+        train_loss = train_loop(model, opt, loss_fn, train_dataloader, device)
         train_loss_list.append(train_loss)
 
-        validation_loss = validation_loop(model, loss_fn, val_dataloader)
+        validation_loss = validation_loop(model, loss_fn, val_dataloader, device)
         validation_loss_list.append(validation_loss)
 
         print(f"Training loss: {train_loss:.4f}")
         print(f"Validation loss: {validation_loss:.4f}")
+        print()
 
     return train_loss_list, validation_loss_list
