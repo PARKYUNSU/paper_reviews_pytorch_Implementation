a135a5559de4951c4f3ac0ab4e0e5a464c4866ed
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-01-17 19:32:48 +0900

    Trnasformer Encoder

--- model/transformer_encoder.py
+++ model/transformer_encoder.py
@@ -3,11 +3,13 @@ import torch.nn as nn
 from .positional_encoding import PositionalEncoding
 from .encoder_layer import Encoder_Layer
 
+# transformer_encoder.py
+
 class TransformerEncoder(nn.Module):
     def __init__(self, num_layers, d_model, num_heads, d_ff, vocab_size, max_seq_len, dropout=0.1):
         super(TransformerEncoder, self).__init__()
         self.embedding = nn.Embedding(vocab_size, d_model)
-        self.positional_encoding = PositionalEncoding(d_model, max_seq_len)
+        self.positional_encoding = PositionalEncoding(d_model, dropout, max_seq_len)  # max_seq_len 전달
         self.layers = nn.ModuleList([Encoder_Layer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
         self.dropout = nn.Dropout(dropout)
 
