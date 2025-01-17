02664b13fb95cacd27581deb5201b896a4e8c83f
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-01-17 19:34:11 +0900

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import torch
-import torch.nn as nn
-from model.transformer import Transformer
+from model import Transformer
 from data import generate_random_data, batchify_data
 from train import fit
 
@@ -11,11 +10,10 @@ num_layers = 3
 d_model = 8
 num_heads = 2
 d_ff = 16
-vocab_size = 4  # 실제로는 vocab_size가 이보다 클 수 있습니다.
+vocab_size = 4  # vocab_size는 실제 데이터에 맞게 설정
 max_seq_len = 8
 dropout = 0.1
 
-# 학습 데이터 생성 및 배치화
 train_data = generate_random_data(9000)
 val_data = generate_random_data(3000)
 
@@ -24,14 +22,6 @@ val_dataloader = batchify_data(val_data)
 
 # 모델 인스턴스화
 model = Transformer(num_layers=num_layers, d_model=d_model, num_heads=num_heads, d_ff=d_ff, vocab_size=vocab_size, max_seq_len=max_seq_len, dropout=dropout).to(device)
-def init_weights(m):
-    if isinstance(m, nn.Linear):
-        nn.init.xavier_normal_(m.weight)  # Xavier 초기화
-        if m.bias is not None:
-            nn.init.zeros_(m.bias)  # 바이어스는 0으로 초기화
-
-model.apply(init_weights)
-
 
 # 옵티마이저와 손실 함수 설정
 opt = torch.optim.SGD(model.parameters(), lr=0.01)
