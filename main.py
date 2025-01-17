b2449b84307fea0f4469525e22bb334375431b2b
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-01-17 19:17:06 +0900

    main.py

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ from train import fit
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# 하이퍼파라미터
-num_layers = 6
-d_model = 512
-num_heads = 8
-d_ff = 2048
-vocab_size = 10000  # vocab_size는 모델에서 사용될 토큰 수
-max_seq_len = 100
+# 하이퍼파라미터 설정
+num_layers = 3
+d_model = 8
+num_heads = 2
+d_ff = 16
+vocab_size = 4  # 실제로는 vocab_size가 이보다 클 수 있습니다.
+max_seq_len = 8
 dropout = 0.1
 
+# 학습 데이터 생성 및 배치화
+train_data = generate_random_data(9000)
+val_data = generate_random_data(3000)
 
+train_dataloader = batchify_data(train_data)
+val_dataloader = batchify_data(val_data)
+
+# 모델 인스턴스화
 model = Transformer(num_layers=num_layers, d_model=d_model, num_heads=num_heads, d_ff=d_ff, vocab_size=vocab_size, max_seq_len=max_seq_len, dropout=dropout).to(device)
 def init_weights(m):
     if isinstance(m, nn.Linear):
@@ -26,13 +33,7 @@ def init_weights(m):
 model.apply(init_weights)
 
 
-train_data = generate_random_data(9000)
-val_data = generate_random_data(3000)
-
-train_dataloader = batchify_data(train_data)
-val_dataloader = batchify_data(val_data)
-
-# 모델, 옵티마이저, 손실 함수 설정
+# 옵티마이저와 손실 함수 설정
 opt = torch.optim.SGD(model.parameters(), lr=0.01)
 loss_fn = torch.nn.CrossEntropyLoss()
 
