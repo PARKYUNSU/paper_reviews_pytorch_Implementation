131610190fd36b86b059893b10f49e01ff3cb399
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-01-24 23:21:31 +0900

    commit

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,4 @@
 from .utils import *
+from .mlp import MLP
+from .attention import Attention
+from .encoder_block import Transformer_Encoder_Block
---
+++ model/encoder_block.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from attention import Attention
+from mlp import MLP
+
+class Transformer_Encoder_Block(nn.Module):
+    def __init__(self, config):
+        super(Transformer_Encoder_Block, self).__init__()
+        self.norm1 = nn.LayerNorm(config.hidden_size, eps=1e-6)
+        self.attn = Attention(config)
+        self.norm2 = nn.LayerNorm(config.hidden_size, eps=1e-6)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
+        
+        return x
