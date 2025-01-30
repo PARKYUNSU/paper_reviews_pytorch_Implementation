0a029bcad2bcef371cd0915293cdbc23d90061c0
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-01-30 18:48:10 +0900

    commit

--- model/attention.py
+++ model/attention.py
@@ -6,21 +6,20 @@ import math
 class Attention(nn.Module):
     def __init__(self, config, vis=False):
         super(Attention, self).__init__()
-        assert config.hidden_size % config.transformer["num_heads"] == 0, "hiddend_size must be divisible by num_heads"
+        assert config.hidden_size % config.transformer["num_heads"] == 0, "hidden_size must be divisible by num_heads"
         self.vis = vis
         self.num_heads = config.transformer["num_heads"]
         self.hidden_size = config.hidden_size
         self.head_dim = self.hidden_size // self.num_heads
         self.all_head_dim = self.num_heads * self.head_dim
 
-        #self.qkv = nn.Linear(self.hidden_size, self.all_head_dim* 3) # 한 번에 qkv 연산
         self.query_dense = nn.Linear(self.hidden_size, self.all_head_dim)
         self.key_dense = nn.Linear(self.hidden_size, self.all_head_dim)
         self.value_dense = nn.Linear(self.hidden_size, self.all_head_dim)
         
-        self.attn_dropout = nn.Dropout(config.transformer["attention_dropout_rate"])
+        self.attn_dropout = nn.Dropout(config.transformer["attention_dropout_rate"])  # Correct Dropout
         self.output_dense = nn.Linear(self.hidden_size, self.hidden_size)
-        self.proj_dropout = nn.Linear(config.transformer["attention_dropout_rate"])
+        self.proj_dropout = nn.Dropout(config.transformer["attention_dropout_rate"])  # Correct Dropout
         self.softmax = nn.Softmax(dim=-1)
     
     def transpose_for_score(self, x):
@@ -40,6 +39,7 @@ class Attention(nn.Module):
         key = self.transpose_for_score(key)
         value = self.transpose_for_score(value)
 
+        # Attention scores calculation
         attention_scores = torch.matmul(query, key.transpose(-1, -2)) / math.sqrt(self.head_dim)
         attention_probs = self.softmax(attention_scores)
         weights = attention_probs if self.vis else None
