a4fffc94b815476707c40f2eeac50622d1c51127
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-01-31 00:04:49 +0900

    commit

--- data.py
+++ data.py
@@ -1,4 +1,3 @@
-import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
@@ -10,10 +9,10 @@ def cifar_10(batch_size=64):
     ])
 
     # CIFAR-10
-    trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    traindset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
-    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(traindset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader
