84a2023b8df9dd584fe48cb47e94c01b358a55d2
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-01-30 13:07:34 +0900

    commit

---
+++ data.py
@@ -0,0 +1,19 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+def cifar_10(batch_size=64):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    # CIFAR-10
+    traindset = datasets.CIFAR10(root='./data', train=True, dowmload=True, transform=transform)
+    testset = datasets.CIFAR10(root='./data', train=False, dowmload=True, transform=transform)
+
+    train_loader = DataLoader(traindset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
