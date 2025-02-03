59e17e71560e901079fb14887c99773ab2f3f9e8
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-02-03 13:26:33 +0900

    commit

--- data.py
+++ data.py
@@ -3,17 +3,23 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
 def cifar_10(batch_size=64):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    # CIFAR-10
-    trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+    trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
+    testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
+
+    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
+    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
 
     return train_loader, test_loader
