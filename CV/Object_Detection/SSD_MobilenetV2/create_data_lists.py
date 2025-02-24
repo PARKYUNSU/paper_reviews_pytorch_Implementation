273586864de7292171d7f8c4f3a928786e4891af
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-02-24 22:29:23 +0900

    commit

--- CV/Object_Detection/SSD_MobilenetV2/create_data_lists.py
+++
@@ -1,6 +0,0 @@
-from utils import create_data_lists
-
-if __name__ == '__main__':
-    create_data_lists(voc07_path='/kaggle/input/pascal/archive/VOC2007',
-                      voc12_path='/kaggle/input/pascal/archive/VOC2012',
-                      output_folder='/kaggle/working/')
