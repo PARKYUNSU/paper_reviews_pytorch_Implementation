8b0888f773291d4e18af716a6f905bfea669e4f3
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-03-01 00:23:20 +0900

    commit

---
+++ Vision Language/CLIP/clip.py
@@ -0,0 +1,53 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from typing import Tuple, Union
+from model import ModifiedResnet, VisionTransformer, Transformer, LayerNorm
+
+class CLIP(nn.Module):
+    def __init__(self, embed_dim: int, image_resolution: int,
+                 vision_layers: Union[Tuple[int, int, int, int], int],
+                 vision_width: int, vision_patch_size: int,
+                 context_length: int, vocab_size: int,
+                 transformer_width: int, transformer_heads: int,
+                 transformer_layers: int):
+        super().__init__()
+
+        self.context_length = context_length
+
+        if isinstance(vision_layers, (tuple, list)):
+            vision_heads = vision_width * 32 // 64
+            self.visual = ModifiedResnet(
+                layers=vision_layers,
+                output_dim=embed_dim,
+                heads=vision_heads,
+                input_resolution=image_resolution,
+                width=vision_width
+            )
+        else:
+            vision_heads = vision_width // 64
+            self.visual = VisionTransformer(
+                input_resolution=image_resolution,
+                patch_size=vision_patch_size,
+                width=vision_width,
+                layers=vision_layers,
+                heads=vision_heads,
+                output_dim=embed_dim
+            )
+
+        self.transformer = Transformer(
+            width=transformer_width,
+            layers=transformer_layers,
+            heads=transformer_heads,
+            attn_mask=self.build_attention_mask()
+        )
+
+        self.vocab_size = vocab_size
+        self.token_embedding = nn.Embedding(vocab_size, transformer_width)
+        self.positional_embedding = nn.Parameter(torch.empty(self.context_length, transformer_width))
+        self.ln_final = LayerNorm(transformer_width)
+
+        self.text_projection = nn.Parameter(torch.empty(transformer_width, embed_dim))
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+
+        self.initialize_parameters()
---
+++ Vision Language/CLIP/model/__init__.py
@@ -0,0 +1,5 @@
+from attnblock import AttentionBlock
+from attnpool import AttentionPool2d
+from resnet import ModifiedResnet
+from utils import LayerNorm, QuickGELU
+from vit import VisionTransformer, Transformer
